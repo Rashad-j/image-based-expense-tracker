364e5b1a5751246157222f2d2de14eefa61b1900
Rashad <s8raalbo@stud.uni-saarland.de>
2024-11-26 15:31:26 +0100

    chore(expenses): update the expenses model

--- internal/core/expense/models.go
+++ internal/core/expense/models.go
@@ -3,8 +3,9 @@ package expenses
 // Expenses represents the output structure.
 type Expenses struct {
 	Items []struct {
-		Name  string `json:"name"`
-		Price string `json:"price"`
+		Name  string  `json:"name"`
+		Price float32 `json:"price"`
 	} `json:"items"`
-	Total string `json:"total"`
+	Total    float32 `json:"total"`
+	Category string  `json:"category"`
 }
